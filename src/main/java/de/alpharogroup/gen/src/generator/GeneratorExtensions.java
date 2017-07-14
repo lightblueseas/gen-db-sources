a0ce0ccdbdbbec5cde76439ab6c56055f195b86f
Asterios Raptis <asterios.raptis@gmx.net>
2017-07-14 16:05:39 +0200

    new generation models created

--- src/main/java/de/alpharogroup/gen/src/generator/GeneratorExtensions.java
+++ src/main/java/de/alpharogroup/gen/src/generator/GeneratorExtensions.java
@@ -18,6 +18,7 @@ import de.alpharogroup.file.FileSuffix;
 import de.alpharogroup.file.create.CreateFileExtensions;
 import de.alpharogroup.file.read.ReadFileExtensions;
 import de.alpharogroup.gen.src.model.ClassGenerationModel;
+import de.alpharogroup.gen.src.model.PomGenerationBean;
 import de.alpharogroup.gen.src.model.RepositoryClassModel;
 import de.alpharogroup.lang.ClassExtensions;
 import de.alpharogroup.lang.PackageExtensions;
@@ -29,7 +30,8 @@ import de.alpharogroup.xml.XmlExtensions;
  * The class {@link GeneratorExtensions} generates repositories, service and unit test classes from
  * the model classes that are in the gen.res.model package.
  */
-public class GeneratorExtensions {
+public class GeneratorExtensions
+{
 
 	/**
 	 * The main method.
@@ -39,10 +41,12 @@ public class GeneratorExtensions {
 	 * @throws Exception
 	 *             the exception
 	 */
-	public static void main(final String[] args) throws Exception {
-		generateRepositoryClasses();
+	public static void main(final String[] args) throws Exception
+	{
+		// generateRepositoryClasses();
 
 	}
+
 
 	/**
 	 * Generate dao classes.
@@ -50,14 +54,16 @@ public class GeneratorExtensions {
 	 * @throws Exception
 	 *             the exception
 	 */
-	public static void generateRepositoryClasses() throws Exception {
+	public static void generateRepositoryClasses() throws Exception
+	{
 		final String classGenerationModel = "ClassGenerationModel.xml";
 
 		final ClassGenerationModel generationData = loadClassGenerationModel(classGenerationModel);
 
 		initializeQualifiedModelClassNames(generationData);
 
-		final List<RepositoryClassModel> repositoryModels = getRepositoryClassModels(generationData);
+		final List<RepositoryClassModel> repositoryModels = getRepositoryClassModels(
+			generationData);
 
 		generateClasses(generationData, repositoryModels);
 
@@ -70,27 +76,73 @@ public class GeneratorExtensions {
 	 *            the class generation model
 	 * @return the class generation model
 	 */
-	public static ClassGenerationModel loadClassGenerationModel(
-			final String classGenerationModel) {
+	public static ClassGenerationModel loadClassGenerationModel(final String classGenerationModel)
+	{
 		final InputStream is = ClassExtensions.getResourceAsStream(classGenerationModel);
 		final String xml = ReadFileExtensions.inputStream2String(is);
 		final ClassGenerationModel generationData = XmlExtensions.toObjectWithXStream(xml);
 		return generationData;
 	}
 
+
 	/**
-	 * Initialize qualified model class names.
+	 * Load class generation model.
+	 *
+	 * @param classGenerationModel
+	 *            the class generation model
+	 * @return the class generation model
+	 */
+	public static PomGenerationBean loadPomGenerationModel(final String classGenerationModel)
+	{
+		final InputStream is = ClassExtensions.getResourceAsStream(classGenerationModel);
+		final String xml = ReadFileExtensions.inputStream2String(is);
+		final PomGenerationBean generationData = XmlExtensions.toObjectWithXStream(xml);
+		return generationData;
+	}
+
+	/**
+	 * Reads the qualified entity class names from the entities in the specified package.
 	 *
 	 * @param generationData
 	 *            the generation data
 	 * @throws Exception
 	 *             the exception
 	 */
-	public static void initializeQualifiedModelClassNames(
-			final ClassGenerationModel generationData) throws Exception {
-		final Set<String> qualifiedModelClassNames =
-			PackageExtensions.scanClassNames(generationData.getModelPackageName());
+	public static void initializeQualifiedModelClassNames(final ClassGenerationModel generationData)
+		throws Exception
+	{
+		final Set<String> qualifiedModelClassNames = PackageExtensions
+			.scanClassNames(generationData.getModelPackageName());
 		generationData.setQualifiedModelClassNames(qualifiedModelClassNames);
+	}
+
+	/**
+	 * Generate pom files.
+	 *
+	 * @throws Exception
+	 *             the exception
+	 */
+	public static void generatePomFiles() throws Exception
+	{
+		final String pomGenerationModel = "PomGenerationModel.xml";
+
+		final PomGenerationBean generationData = loadPomGenerationModel(pomGenerationModel);
+		final VelocityContext context = new VelocityContext();
+		context.put("model", generationData);
+
+		final Template businessPomTemplate = Velocity.getTemplate(generationData.getBusinessPom());
+
+		final String businessPomClassPath = generationData.getParentName() + "/"
+			+ generationData.getParentName() + "-business" + "/" + "pom.xml";
+		final File f = new File(businessPomClassPath);
+		CreateFileExtensions.newFileQuietly(f);
+		final BufferedWriter writer = new BufferedWriter(new FileWriter(businessPomClassPath));
+
+		businessPomTemplate.merge(context, writer);
+		writer.flush();
+		writer.close();
+
+
 	}
 
 	/**
@@ -104,18 +156,20 @@ public class GeneratorExtensions {
 	 *             the IO exception
 	 */
 	public static void generateClasses(final ClassGenerationModel generator,
-			final List<RepositoryClassModel> repositoryModels) throws IOException {
-		for (final RepositoryClassModel model : repositoryModels) {
+		final List<RepositoryClassModel> repositoryModels) throws IOException
+	{
+		for (final RepositoryClassModel model : repositoryModels)
+		{
 
 			final VelocityContext context = new VelocityContext();
 			context.put("model", model);
 
-			final Template repositoryClassTemplate = Velocity.getTemplate(generator
-					.getRepositoryClassTemplateFile());
+			final Template repositoryClassTemplate = Velocity
+				.getTemplate(generator.getRepositoryClassTemplateFile());
 
 			final String repositoryClassPath = generator.getSrcFolder()
-					+ generator.getSrcGenerationPackage()
-					+ model.getRepositoryClassName() + FileExtension.JAVA.getExtension();
+				+ generator.getSrcGenerationPackage() + model.getRepositoryClassName()
+				+ FileExtension.JAVA.getExtension();
 			File f = new File(repositoryClassPath);
 			CreateFileExtensions.newFileQuietly(f);
 			final BufferedWriter writer = new BufferedWriter(new FileWriter(repositoryClassPath));
@@ -124,49 +178,45 @@ public class GeneratorExtensions {
 			writer.flush();
 			writer.close();
 
-			final Template daoTestClassTemplate = Velocity.getTemplate(generator
-					.getRepositoryTestClassTemplateFile());
+			final Template daoTestClassTemplate = Velocity
+				.getTemplate(generator.getRepositoryTestClassTemplateFile());
 			final String daoTestClassPath = generator.getSrcTestFolder()
-					+ generator.getSrcTestGenerationPackage()
-					+ model.getRepositoryClassName()  + FileSuffix.TEST
-					+ FileExtension.JAVA.getExtension();
+				+ generator.getSrcTestGenerationPackage() + model.getRepositoryClassName()
+				+ FileSuffix.TEST + FileExtension.JAVA.getExtension();
 			f = new File(daoTestClassPath);
 			CreateFileExtensions.newFileQuietly(f);
 			final BufferedWriter daoTestClassWriter = new BufferedWriter(
-					new FileWriter(daoTestClassPath));
+				new FileWriter(daoTestClassPath));
 
 			daoTestClassTemplate.merge(context, daoTestClassWriter);
 			daoTestClassWriter.flush();
 			daoTestClassWriter.close();
 
-			final Template serviceInterfaceTemplate = Velocity.getTemplate(generator
-					.getServiceInterfaceTemplateFile());
-			final String serviceInterfaceClassPath =generator.getSrcFolder()
-					+ generator.getSrcServiceGenerationPackage()
-					+ model.getServiceClassName() + FileExtension.JAVA.getExtension();
+			final Template serviceInterfaceTemplate = Velocity
+				.getTemplate(generator.getServiceInterfaceTemplateFile());
+			final String serviceInterfaceClassPath = generator.getSrcFolder()
+				+ generator.getSrcServiceGenerationPackage() + model.getServiceClassName()
+				+ FileExtension.JAVA.getExtension();
 			f = new File(serviceInterfaceClassPath);
 			CreateFileExtensions.newFileQuietly(f);
 			final BufferedWriter serviceInterfaceWriter = new BufferedWriter(
-					new FileWriter(generator.getSrcFolder()
-							+ generator.getSrcServiceGenerationPackage()
-							+ model.getServiceClassName() + FileExtension.JAVA.getExtension()));
+				new FileWriter(generator.getSrcFolder() + generator.getSrcServiceGenerationPackage()
+					+ model.getServiceClassName() + FileExtension.JAVA.getExtension()));
 
 			serviceInterfaceTemplate.merge(context, serviceInterfaceWriter);
 			serviceInterfaceWriter.flush();
 			serviceInterfaceWriter.close();
-			final Template serviceClassTemplate = Velocity.getTemplate(generator
-					.getServiceClassTemplateFile());
-			final String serviceClassPath =generator.getSrcFolder()
-					+ generator.getSrcServiceGenerationPackage()
-					+ model.getModelClassName() + "Business"
-					+ FileSuffix.SERVICE + FileExtension.JAVA.getExtension();
+			final Template serviceClassTemplate = Velocity
+				.getTemplate(generator.getServiceClassTemplateFile());
+			final String serviceClassPath = generator.getSrcFolder()
+				+ generator.getSrcServiceGenerationPackage() + model.getModelClassName()
+				+ "Business" + FileSuffix.SERVICE + FileExtension.JAVA.getExtension();
 			f = new File(serviceClassPath);
 			CreateFileExtensions.newFileQuietly(f);
 			final BufferedWriter serviceClassWriter = new BufferedWriter(
-					new FileWriter(generator.getSrcFolder()
-							+ generator.getSrcServiceGenerationPackage()
-							+ model.getModelClassName() + "Business"
-							+ FileSuffix.SERVICE + FileExtension.JAVA.getExtension()));
+				new FileWriter(generator.getSrcFolder() + generator.getSrcServiceGenerationPackage()
+					+ model.getModelClassName() + "Business" + FileSuffix.SERVICE
+					+ FileExtension.JAVA.getExtension()));
 
 			serviceClassTemplate.merge(context, serviceClassWriter);
 			serviceClassWriter.flush();
@@ -183,24 +233,32 @@ public class GeneratorExtensions {
 	 */
 	@SuppressWarnings({ "rawtypes", "unchecked" })
 	public static List<RepositoryClassModel> getRepositoryClassModels(
-			final ClassGenerationModel generator) {
+		final ClassGenerationModel generator)
+	{
 		final List<RepositoryClassModel> repositoryModels = new ArrayList<>();
-		for (final String clazz : generator.getQualifiedModelClassNames()) {
+		for (final String clazz : generator.getQualifiedModelClassNames())
+		{
 			final RepositoryClassModel model = new RepositoryClassModel();
 
 			final String modelClassName = clazz.substring(clazz.lastIndexOf(".") + 1,
-					clazz.length());
-			if (modelClassName.equals("package-info")) {
+				clazz.length());
+			if (modelClassName.equals("package-info"))
+			{
 				continue;
 			}
 
-			try {
+			try
+			{
 				final Class modelClass = Class.forName(clazz);
-				final Class primaryKeyClassType = TypeArgumentsExtensions.getFirstTypeArgument(ClassExtensions.getBaseClass(modelClass), modelClass);
+				final Class primaryKeyClassType = TypeArgumentsExtensions
+					.getFirstTypeArgument(ClassExtensions.getBaseClass(modelClass), modelClass);
 
 				model.setPrimaryKeyClassName(primaryKeyClassType.getName());
-			} catch (final ClassNotFoundException e) {
-				throw new RuntimeException(e.getMessage()+ "\n by get the first type argument from the entity class "+ modelClassName);
+			}
+			catch (final ClassNotFoundException e)
+			{
+				throw new RuntimeException(e.getMessage()
+					+ "\n by get the first type argument from the entity class " + modelClassName);
 			}
 
 
@@ -221,13 +279,14 @@ public class GeneratorExtensions {
 			model.setRepSpringRefClassName(repSpringRefClassName);
 			model.setRepositoryClassName(repositoryClassName);
 			model.setModelClassName(modelClassName);
-			model.setModelInstanceName(StringExtensions
-				.firstCharacterToLowerCase(model.getModelClassName()));
+			model.setModelInstanceName(
+				StringExtensions.firstCharacterToLowerCase(model.getModelClassName()));
 		}
 		return repositoryModels;
 	}
 
-	public static void generatePoms() {
+	public static void getPomGenerationModel(final PomGenerationBean generator)
+	{
 
 	}
 
---
+++ src/main/java/de/alpharogroup/gen/src/model/PomGenerationBean.java
@@ -0,0 +1,31 @@
+package de.alpharogroup.gen.src.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@EqualsAndHashCode
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder(toBuilder = true)
+public class PomGenerationBean
+{
+	private String parentName;
+    // pom files
+    private String businessPom;
+    private String dataPom;
+    private String domainPom;
+    private String entitiesPom;
+    private String initPom;
+    private String restApiPom;
+    private String restClientPom;
+    private String restWebPom;
+
+}
